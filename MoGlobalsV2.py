955cf99687d0c1234cf781e2f376090eb1848b0d
mehmckeHOME <mehmcke@gmx.net>
2023-03-25 19:12:32 +0100

    added unique clipping-values for viewport/cameras

---
+++ MoGlobalsV2.py
@@ -0,0 +1,98 @@
+bl_info = {
+    "name" : "Mo Globals",
+    "blender" : (3, 3, 0),
+    "location" : "View3D",
+    "category" : "3D View"
+}
+
+import bpy
+from bpy.types import Operator
+from bpy.types import Panel
+
+class MoChangeClippingValues(Operator):
+    bl_idname = "mo.change_clipping_values"
+    bl_label = "Change Clipping Values"
+    bl_options = {"REGISTER", "UNDO"}
+
+    @classmethod
+    def poll(cls, context):
+        return context.mode == "OBJECT"
+
+    def execute(self, context):
+        scene = context.scene
+        props = scene.MoGlobalsProps
+        if props.viewport_clip_start < props.viewport_clip_end and props.camera_clip_start < props.camera_clip_end:
+            # Get every viewport from all workspaces and set clipping values
+            for workspace in bpy.data.workspaces:
+                for screen in workspace.screens:
+                    for area in screen.areas:        
+                        if area.type == "VIEW_3D":
+                                area.spaces.active.clip_start = props.viewport_clip_start
+                                area.spaces.active.clip_end = props.viewport_clip_end
+                  
+            # Get every camera and set clipping values
+            for camera in bpy.data.cameras:
+                camera.clip_start = props.camera_clip_start
+                camera.clip_end = props.camera_clip_end
+            return {'FINISHED'}
+        else:
+            return{'CANCELLED'}
+
+class GLOBALS_PT_MoGlobalSettings(Panel):
+    bl_label = "Globals"
+    bl_idname = 'globals.ui'
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_category = "Globals"    
+    bl_context = "objectmode"    
+    
+    def draw(self, context):
+        layout = self.layout
+        scene = context.scene
+        layout.label(text="Viewport Clipping:")
+        layout.prop(scene.MoGlobalsProps,'viewport_clip_start')
+        layout.prop(scene.MoGlobalsProps,'viewport_clip_end')
+        layout.label(text="Camera Clipping:")
+        layout.prop(scene.MoGlobalsProps,'camera_clip_start')
+        layout.prop(scene.MoGlobalsProps,'camera_clip_end')
+        col = layout.column(align=True)
+        prop = col.operator(MoChangeClippingValues.bl_idname, text="Change Clipping Values")
+
+
+class MoGlobalsProperties(bpy.types.PropertyGroup):
+    camera_clip_start: bpy.props.FloatProperty(name = 'Camera Clip Start',
+                    default = 0.1,
+                    min = 0.0)
+                    
+    camera_clip_end: bpy.props.FloatProperty(name = 'Camera Clip End',
+                    default = 1000.0,
+                    min = 0.0)
+                    
+    viewport_clip_start: bpy.props.FloatProperty(name = 'Viewport Clip Start',
+                    default = 0.1,
+                    min = 0.0)
+                    
+    viewport_clip_end: bpy.props.FloatProperty(name = 'Viewport Clip End',
+                    default = 5000.0,
+                    min = 0.0)
+    
+classes = [
+    MoChangeClippingValues,
+    GLOBALS_PT_MoGlobalSettings,
+    MoGlobalsProperties,
+]
+
+def register():
+    for c in classes:
+        bpy.utils.register_class(c)
+    bpy.types.Scene.MoGlobalsProps = bpy.props.PointerProperty(type = MoGlobalsProperties)
+
+
+def unregister():
+    for c in classes:
+        bpy.utils.unregister_class(c)
+    del bpy.types.Scene.MoGlobalsProps
+
+
+if __name__ == '__main__':
+    register()
